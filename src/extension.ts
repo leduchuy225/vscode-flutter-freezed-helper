8525989c8b8a7ccbc50b0c7e5d8a01278682ff52
Huy LĐ <leduchuy2205@gmail.com>
2023-11-10 22:06:31 +0700

    Update code

--- src/extension.ts
+++ src/extension.ts
@@ -1,25 +1,32 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
+import * as fs from "fs";
 import * as vscode from "vscode";
-import { exec, spawn } from "child_process";
+import { spawn } from "child_process";
 
-const handleFileName = (name: string) => {
+const handleFileName = (name: string, isFolder: boolean) => {
+  if (isFolder) {
+    return name + "/**";
+  }
+
   const partName = name.split(".");
   partName.splice(partName.length - 1, 0, "*");
 
   return partName.join(".");
 };
 
-// const handleOutput = (data: string) => {
-//   const partData = data.split("\n");
-//   return partData[partData.length - 1];
-// };
+const handleOutput = (data: string) => {
+  const partData = data.split("[INFO]");
+  return partData[partData.length - 1].trim();
+};
 
 export function deactivate() {}
 
 export function activate(context: vscode.ExtensionContext) {
-  const buildRunnerSingleFile = (uri: vscode.Uri) => {
+  const buildRunnerOnClick = (uri: vscode.Uri) => {
     console.log(uri);
+
+    const isForlder = fs.lstatSync(uri.fsPath).isDirectory();
 
     const filePath = uri.fsPath;
 
@@ -27,50 +34,56 @@ export function activate(context: vscode.ExtensionContext) {
 
     const path = filePath.substring(libIndex);
     const partition = path.split("/");
-    const fileName = handleFileName(partition[partition.length - 1]);
+
+    const fileName = handleFileName(partition[partition.length - 1], isForlder);
 
     partition.pop();
     partition.push(fileName);
 
     const newPath = partition.join("/");
 
-    console.log(vscode.workspace.workspaceFile);
-
     const cwd = filePath.substring(0, libIndex);
 
     console.log(cwd);
     console.log(newPath);
 
-    // exec(
-    //   `${cwd} flutter pub run build_runner build --delete-conflicting-outputs --build-filter='${newPath}'`,
-    //   (err, stdout, stderr) => {
-    //     console.log("stdout: " + stdout);
-    //     if (stderr) {
-    //       vscode.window.showErrorMessage(stderr);
-    //     }
-    //     if (err) {
-    //       console.log("error: " + err);
-    //     }
-    //   }
-    // );
+    vscode.window.withProgress(
+      {
+        cancellable: false,
+        title: "Build_runner is running...",
+        location: vscode.ProgressLocation.Notification,
+      },
+      async (
+        progress: vscode.Progress<{ message?: string; increment?: number }>,
+        _: vscode.CancellationToken
+      ) => {
+        progress.report({ increment: 0 });
 
-    const process = spawn(
-      `flutter pub run build_runner build --delete-conflicting-outputs --build-filter='${newPath}'`,
-      { shell: true, cwd: cwd }
+        const process = spawn(
+          `flutter pub run build_runner build --build-filter='${newPath}'`,
+          { shell: true, cwd: cwd }
+        );
+
+        let result = "";
+        process.stdout.on("data", (data) => {
+          progress.report({ message: data.toString() });
+          result += data.toString();
+        });
+        process.on("close", (_) => {
+          console.log(result);
+
+          progress.report({ increment: 100 });
+          vscode.window.showInformationMessage(handleOutput(result));
+        });
+      }
     );
-    process.stdout.on("data", (data) => {
-      vscode.window.showInformationMessage(data);
-    });
-    process.stderr.on("data", (data) => {
-      vscode.window.showErrorMessage(data);
-    });
   };
 
   context.subscriptions.push(
     vscode.commands.registerCommand(
-      "flutterBuildRunnerHelper.buildRunnerSingleFile",
+      "flutterBuildRunnerHelper.buildRunnerOnClick",
       (clickedFile: vscode.Uri) => {
-        buildRunnerSingleFile(clickedFile);
+        buildRunnerOnClick(clickedFile);
       }
     )
   );
